3f488f0cfa34adadcf3a0a652596fca48d32c87f
rpsin <rpsinghparihar2000@gmail.com>
2022-11-06 14:54:11 +0530

    yaml updated

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+
+@app.route("/",methods =['GET','POST'])
+def index():
+    return "Starting machine learning project"
+
+if __name__=="__main__":
+    app.run(debug=True)

b2519fae20f22e32b98077398af5d95fef8ecc85
rpsin <rpsinghparihar2000@gmail.com>
2022-11-06 16:22:23 +0530

    testing and deploying

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/",methods =['GET','POST'])
 def index():
-    return "Starting machine learning project"
+    return "Hi folks , Myself Roodra I AM COMING TO MAKE YOU BLOW AWAY"
 
 if __name__=="__main__":
     app.run(debug=True)
